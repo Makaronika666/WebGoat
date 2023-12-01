7f13fbff019ce17439d0b6355578a69cc6fb1fbb
Veronika Svientik <105884243+Makaronika666@users.noreply.github.com>
2023-12-01 10:33:03 +0100

    Update HashingAssignment.java changed MD5 to SHA-256

--- webgoat-lessons/crypto/src/main/java/org/owasp/webgoat/crypto/HashingAssignment.java
+++ webgoat-lessons/crypto/src/main/java/org/owasp/webgoat/crypto/HashingAssignment.java
@@ -38,31 +38,41 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Random;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+import javax.xml.bind.DatatypeConverter;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @AssignmentHints({"crypto-hashing.hints.1","crypto-hashing.hints.2"})
 public class HashingAssignment extends AssignmentEndpoint {
 	
-	public static final String[] SECRETS = {"secret","admin","password", "123456", "passw0rd"};
-
-	@RequestMapping(path="/crypto/hashing/md5",produces=MediaType.TEXT_HTML_VALUE)
+	  @RequestMapping(path = "/crypto/hashing/sha256", produces = MediaType.TEXT_HTML_VALUE)
     @ResponseBody
-    public String getMd5(HttpServletRequest request) throws NoSuchAlgorithmException {
-		
-		String md5Hash = (String) request.getSession().getAttribute("md5Hash");
-		if (md5Hash == null) {
-			
-			String secret = SECRETS[new Random().nextInt(SECRETS.length)];
-	         
-		    MessageDigest md = MessageDigest.getInstance("MD5");
-		    md.update(secret.getBytes());
-		    byte[] digest = md.digest();
-		    md5Hash = DatatypeConverter
-		      .printHexBinary(digest).toUpperCase();
-			request.getSession().setAttribute("md5Hash", md5Hash);
-			request.getSession().setAttribute("md5Secret", secret);
-		}
-		return md5Hash;
+    public String getSha256(HttpServletRequest request) throws NoSuchAlgorithmException {
+        String sha256Hash = (String) request.getSession().getAttribute("sha256Hash");
+        if (sha256Hash == null) {
+            SecureRandom random = new SecureRandom();
+            byte[] salt = new byte[16];
+            random.nextBytes(salt);
+            String secret = Base64.getEncoder().encodeToString(salt);
+
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(secret.getBytes());
+            byte[] digest = md.digest();
+            sha256Hash = DatatypeConverter.printHexBinary(digest).toUpperCase();
+
+            request.getSession().setAttribute("sha256Hash", sha256Hash);
+            request.getSession().setAttribute("sha256Secret", secret);
+        }
+        return sha256Hash;
     }
+}
 	
 	@RequestMapping(path="/crypto/hashing/sha256",produces=MediaType.TEXT_HTML_VALUE)
     @ResponseBody
